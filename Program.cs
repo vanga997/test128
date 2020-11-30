20107d792ba7fb20651a581360efb90cbd3d51d1
б а <promtman1990@gmail.com>
2020-11-30 09:31:17 +0300

    entity classes generated

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test128
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
